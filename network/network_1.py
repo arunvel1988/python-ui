ea7181910acacb912eb048807f3aeba3f49eb6ea
ARUN <aryan.arunachalam@gmail.com>
2026-05-21 04:10:02 -0400

    Add memory usage calculation for network router

--- network/network_1.py
+++ network/network_1.py
@@ -1,1 +1,13 @@
 print("test network:")
+router_name = "Core-Router-1"
+total_memory = 32
+used_memory = 18
+
+available_memory = total_memory - used_memory
+memory_usage_percentage = (used_memory / total_memory) * 100
+
+print("Device Name :", router_name)
+print("Total Memory :", total_memory, "GB")
+print("Used Memory :", used_memory, "GB")
+print("Available Memory :", available_memory, "GB")
+print("Memory Usage :", memory_usage_percentage, "%")
