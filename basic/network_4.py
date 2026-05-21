692f599acebe6f0bcf948352820b46a90cf8800d
ARUN <aryan.arunachalam@gmail.com>
2026-05-21 03:38:16 -0400

    Create network_4.py

---
+++ basic/network_4.py
@@ -0,0 +1,13 @@
+devices = [
+    "192.168.1.1",
+    "192.168.1.2",
+    "192.168.1.3",
+    "192.168.1.4"
+]
+
+print("Starting Network Scan...\n")
+
+for ip in devices:
+    print("Pinging Device :", ip)
+
+print("\nNetwork Scan Completed")
