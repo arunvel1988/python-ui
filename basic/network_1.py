6df39fc2e98b397623e897585a793a5914b754a1
ARUN <aryan.arunachalam@gmail.com>
2026-05-21 03:26:49 -0400

    Create network_1.py

---
+++ basic/network_1.py
@@ -0,0 +1,15 @@
+# Variables
+
+router_name = "CoreRouter1"
+ip_address = "192.168.1.1"
+port_count = 24
+cpu_usage = 67.5
+is_active = True
+
+# Display values
+
+print("Router Name:", router_name)
+print("IP Address:", ip_address)
+print("Ports:", port_count)
+print("CPU Usage:", cpu_usage)
+print("Router Status:", is_active)
