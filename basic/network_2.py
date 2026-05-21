dc5c4258a498524a8382a3b81368432b0363601a
ARUN <aryan.arunachalam@gmail.com>
2026-05-21 03:32:52 -0400

    Add basic network configuration script

---
+++ basic/network_2.py
@@ -0,0 +1,10 @@
+switch_name = input("Enter Switch Name: ")
+port_number = input("Enter Port Number: ")
+vlan_id = input("Enter VLAN ID: ")
+
+print("\nApplying Configuration...")
+print("Switch :", switch_name)
+print("Port :", port_number)
+print("Assigned VLAN :", vlan_id)
+
+print("\nConfiguration Completed Successfully")
